624ca8b1668c384f85a7d984df799ee925901dda
fumitti <fumitti@gmail.com>
2016-08-18 19:50:47 +0900

    very simple slack intg

--- JpegProgressiveChecker/Program.cs
+++ JpegProgressiveChecker/Program.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using Codeplex.Data;
 
 namespace JpegProgressiveChecker
 {
@@ -15,7 +17,12 @@ namespace JpegProgressiveChecker
             foreach (var s in args)
             {
                 //Console.WriteLine(s);
-                if (Directory.Exists(s))
+                if (s.Contains("https://hooks.slack.com/"))
+                {
+                    enableSlack = true;
+                    slackIntgUrl = s.Replace("\"","");
+                }
+                else if (Directory.Exists(s))
                 {
                     foreach (var file in Directory.EnumerateFiles(s, "*.jpg", SearchOption.AllDirectories))
                     {
@@ -32,6 +39,8 @@ namespace JpegProgressiveChecker
         }
 
         private const int Bufsize = 4096;//?
+        private static bool enableSlack = false;
+        private static string slackIntgUrl = "";
 
         //32    slow
         //64    1:19
@@ -69,7 +78,26 @@ namespace JpegProgressiveChecker
         static async Task CheckJpegProgressive(string filePath)
         {
             if (await IsJpegProgressive(filePath))
+            {
                 Console.Out.WriteLine(filePath + " is Progressive Jpeg.");
+                if (enableSlack)
+                {
+                    using (var client = new HttpClient())
+                    {
+                        var att = new
+                        {
+                            color = "#FF0000",
+                            text = filePath + " is Progressive Jpeg.",
+                        };
+                        var data = DynamicJson.Serialize(new
+                        {
+                            text = "",
+                            attachments = new object[] { att },
+                        });
+                        await client.PostAsync(slackIntgUrl, new StringContent(data));
+                    }
+                }
+            }
             //Console.Out.WriteLine(filePath + " is BaseLine Jpeg.");
         }
     }
